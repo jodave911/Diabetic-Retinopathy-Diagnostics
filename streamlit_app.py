aaae7b97b31fcb02cf22fdbf57f675c819901bed
Joshua David <62954129+jodave911@users.noreply.github.com>
2024-04-29 00:38:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,6 @@ import streamlit as st
 
 from st_pages import Page, show_pages, add_page_title
 
-add_page_title("Home")
-
-# Specify what pages should be shown in the sidebar, and what their titles 
-# and icons should be
-
 st.set_page_config(
     page_title="Diabetic Retinopathy",
     page_icon="👋",
