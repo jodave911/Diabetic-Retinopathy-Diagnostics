d5d0bc52edfb2829b5994def74d9d4e833a7afea
Joshua David <62954129+jodave911@users.noreply.github.com>
2024-04-28 23:18:01 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,16 +5,16 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Diabetic Retinopathy Diagnosis 👋")
+st.write("# Diabetic Retinopathy Diagnosis 👁️")
 
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    To develop a hybrid image enhancement algorithm using deep learning for accurate diabetic retinopathy diagnosis from fundus images.
+    Our aim is to develop a hybrid image enhancement algorithm using deep learning for accurate diabetic retinopathy diagnosis from fundus images.
    
     - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [Diagnosis](https://diabetic-retinopathy-diagnostics-911.streamlit.app/Diagnostics)
+    - Check eye fundus for DR or NoDR [Diagnosis](https://diabetic-retinopathy-diagnostics-911.streamlit.app/Diagnostics)
     
    
 """
